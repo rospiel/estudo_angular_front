f51f76e5f09bea2f939c7e567b1ccd46c4227b18
rospiel <rospielberg@gmail.com>
2018-01-14 15:18:24 -0200

    Deixando componente Calendar em Português

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoCadastroComponent implements OnInit {
 
+  pt: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.pt = {
+      firstDayOfWeek: 1,
+      dayNames: [ 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado' ],
+      dayNamesShort: [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+      dayNamesMin: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+      monthNames: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+      monthNamesShort: [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+      today: 'Hoje',
+      clear: 'Limpar'
+    };
+
   }
 
 }
