1da497aa18628cad97cf45b398b60dbc98865139
rospiel <rospielberg@gmail.com>
2018-03-22 13:26:54 -0300

    Externalizando as rotas configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,19 +16,9 @@ import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa
 import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
 import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(localePt, 'pt-BR');
-
-const rotas: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  /* Rota para satisfazer a busca de lançamento pelo código */
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
 
 @NgModule({
   declarations: [
@@ -39,7 +29,7 @@ const rotas: Routes = [
     BrowserAnimationsModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(rotas),
+    AppRoutingModule,
 
     LancamentosModule,
     PessoasModule,
