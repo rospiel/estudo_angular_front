a3d2236b91f5f17e4ef776552c8519ad4b64e70a
rospiel <rospielberg@gmail.com>
2018-03-23 20:08:22 -0300

    Fazendo uso da classe de serviço pra login

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,6 +1,10 @@
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AutenticacaoService } from './../autenticacao.service';
 import { BarraAguardeService } from '../../shared/barra-aguarde/BarraAguardeService.service';
+import { ErrorHandlerService } from '../../core/error-handler.service';
 
 @Component({
   selector: 'app-login-form',
@@ -9,10 +13,22 @@ import { BarraAguardeService } from '../../shared/barra-aguarde/BarraAguardeServ
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor(private barraAguardeService: BarraAguardeService) { }
+  constructor(private barraAguardeService: BarraAguardeService,
+              private autenticacaoService: AutenticacaoService,
+              private errorHandle: ErrorHandlerService,
+              private redirecionar: Router) { }
 
   ngOnInit() {
     this.barraAguardeService.esconderBarra();
+  }
+
+  login(usuario: string, senha: string, form: FormControl) {
+    this.autenticacaoService.login(usuario, senha).then(() => {
+      this.redirecionar.navigate(['/lancamentos']);
+    }).catch(erro => {
+      this.errorHandle.handle(erro);
+      form.reset();
+    });
   }
 
 }
