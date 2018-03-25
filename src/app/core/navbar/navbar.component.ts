3ad20853cf39077be7fb49f667010e5dcb55f95e
rospiel <rospielberg@gmail.com>
2018-03-25 12:19:23 -0300

    Fazendo uso do nome do usuário logado no sistema.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AutenticacaoService } from '../../seguranca/autenticacao.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private autenticacaoService: AutenticacaoService) { }
 
   ngOnInit() {
   }
